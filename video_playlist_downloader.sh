fadd14b75f86267da345521251381ecde7e31c03
SANJLA SOREN <sorensanjla092@gmail.com>
2025-03-15 02:51:24 +0000

    Add files via upload

---
+++ video_playlist_downloader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "Enter Playlist url : " playlist_url
+if [ -z "$playlist_url" ];
+then
+ echo "Error : url not provided !"
+fi
+yt-dlp --flat-playlist "$playlist_url"
+read -p "Enter resulation ex- 240, 360 : " resulation
+read -p "Enter starting video no. : " starting
+read -p "Enter ending video no. : " ending
+
+yt-dlp --yes-playlist \
+--playlist-start "$starting" \
+--playlist-end "$ending" \
+-f "bestvideo[height=${resulation}]+bestaudio[ext=m4a]" \
+-o "/sdcard/Downloads/%(playlist)s/%(title)s.%(ext)s" \
+"$playlist_url"
+
